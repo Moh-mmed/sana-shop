79887d575bcc7286bf49665abc95ac965eb57e1c
mohammedBnomr <moohaboy@gmail.com>
2023-02-16 15:11:22 +0300

    fix store bug

--- redux/cartSlice.ts
+++ redux/cartSlice.ts
@@ -6,58 +6,42 @@ import { ProductTypes } from "../types/DataTypes";
 // console.log(Cookies.get('cart'))
 
 const initialState = {
-   cartItems: [{
-      name: 'Fit Shirt',
-      slug: 'fit-shirt',
-      category: 'Shirts',
-      image: '/images/shirt2.jpg',
-      price: 80,
-      brand: 'Adidas',
-      rating: 3.2,
-      numReviews: 10,
-      countInStock: 20,
-      description: 'A popular shirt',
-      isFeatured: true,
-      banner: '/images/banner2.jpg',
-    }],
+    cartItems: [],
     shippingAddress: {},
-    paymentMethod: 'ddd'
+    paymentMethod: ''
+};
+
 //   cart: Cookies.get('cart')
 //     ? JSON.parse('{ cartItems: [], shippingAddress: {}, paymentMethod: "" }')
 //     : { cartItems: [], shippingAddress: {}, paymentMethod: '' },
-};
-
 
 const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    addProduct: (state, action) => {
+    addProduct: (state:any, action) => {
       const newItem = action.payload;
       const existItem = state.cartItems.find(
-        (item:ProductTypes) => item.slug === newItem.slug
+        (item: ProductTypes) => item.slug === newItem.slug
       );
       const cartItems = existItem
         ? state.cartItems.map((item:ProductTypes) =>
             item.name === existItem.name ? newItem : item
         ): [...state.cartItems, newItem];
-          
-    //   Cookies.set('cart', JSON.stringify({ ...state.cart, cartItems }));
-      state = { ...state, cartItems };
+      // Cookies.set('cart', JSON.stringify({ ...state.cart, cartItems }));
+      state.cartItems = cartItems;
     },
     removeProduct: (state, action) => {
       const cartItems = state.cartItems.filter(
         (item:ProductTypes) => item.slug !== action.payload.slug
       );
     //   Cookies.set('cart', JSON.stringify({ ...state.cart, cartItems }));
-      state = { ...state, cartItems };
+       state.cartItems = cartItems;
     },
     reset: (state) => {
-       state = {
-            cartItems: [],
-            shippingAddress: { location: {} },
-            paymentMethod: '',
-        }
+      state.cartItems = []
+      state.shippingAddress = {}
+      state.paymentMethod = ''
     },
   },
 });
