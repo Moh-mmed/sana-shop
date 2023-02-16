818f8c2f7bf1b684327510fcc070c322e4b082dc
mohammedBnomr <moohaboy@gmail.com>
2023-02-16 15:11:52 +0300

    adding product to cart

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,17 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 const Navbar: React.FC = () => {
+
+  const cart = useSelector((state:any) => state.cart);
+  const [cartItemsCount, setCartItemsCount] = useState(0);
+
+  useEffect(() => {
+    setCartItemsCount(cart.cartItems.reduce((a:any, c:any) => a + c.quantity, 0));
+  }, [cart.cartItems]);
+
+
   return (
     <nav className="bg-gray-800">
       <div className="max-w-7xl px-10 sm:px-6 lg:px-8">
@@ -11,16 +22,23 @@ const Navbar: React.FC = () => {
             </Link>
           </div>
           <div className="flex">
-            <Link href="/products" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                Products
+            <Link href="/login" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                login
             </Link>
-            <Link href="/cart" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                Cart
+            <Link href="/cart" className="relative text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+              Cart
+                {cartItemsCount > 0 && (
+                  <span className="absolute top-[-2px] right-[-2px] flex items-center justify-center ml-1 bg-red-600 w-4 h-4 rounded-full text-[10px] leading-4 font-bold text-white">
+                    {cartItemsCount}
+                  </span>
+                )}
             </Link>
           </div>
         </div>
       </div>
     </nav>
+
+    
   );
 };
 
--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import React, { useContext } from 'react';
+import React from 'react';
 import axios from 'axios';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -9,85 +9,90 @@ import { GetServerSideProps, NextPage } from 'next';
 import ProductDetailTypes from '../../types/ProductDetailTypes';
 // import Product from '../../models/Product';
 // import db from '../../utils/db';
-// import { Store } from '../../utils/Store';
+import { useDispatch, useSelector } from "react-redux";
+import { addProduct, reset} from "../../redux/cartSlice";
+import { CartTypes } from '../../types/CartTypes';
+import { ProductTypes } from '../../types/DataTypes';
 
-const ProductDetail: NextPage<ProductDetailTypes> = ({product}) => {
-//   const { state, dispatch } = useContext(Store);
+const ProductDetail: NextPage<ProductDetailTypes> = ({ product }) => {
+  const cart = useSelector((state:any) => state.cart);
+  const dispatch = useDispatch();
   const router = useRouter();
+    
+    // console.log(cart)
   if (!product) {
     return <Layout title="Product Not Found">Product Not Found</Layout>;
   }
 
   const addToCartHandler = async () => {
-    // const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
-    // const quantity = existItem ? existItem.quantity + 1 : 1;
-    const { data } = await axios.get(`/api/products/${product._id}`);
+    const existItem = cart.cartItems.find((x:ProductTypes) => x.slug === product.slug);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    // const { data } = await axios.get(`/api/products/${product._id}`);
 
-    // if (data.countInStock < quantity) {
+      if (product.countInStock < quantity) {
+        return console.log('error')
     //   return toast.error('Sorry. Product is out of stock');
-    // }
-
-    // dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
-    router.push('/cart');
+    }
+    dispatch(addProduct({ ...product, quantity }))
+    // router.push('/cart');
   };
 
   return (
     <Layout title={product.name}>
       <div className="py-4 flex justify-between items-center">
-  <Link href="/" className="text-gray-500 hover:text-gray-700">
-    &lt; Back to Products
-  </Link>
-</div>
-<div className="grid grid-cols-1 md:grid-cols-4 md:gap-4 mb-5">
-  <div className="md:col-span-2">
-    <Image
-      src={product.image}
-      alt={product.name}
-      width={340}
-      height={540}
-      className="object-cover rounded-lg shadow-md mb-6"
-    />
-  </div>
-<div className="col-span-3 md:col-span-2">
-<h1 className="text-3xl font-bold text-gray-800 mb-4">{product.name}</h1>
-    <div className="p-2 text-sm">
-    <p className="text-gray-700 mb-2">
-      Category: <span className='ml-3 text-lg'>{product.category}</span>
-    </p>
-    <p className="text-gray-700 mb-2">
-      Brand: <span className='ml-3 text-lg'>{product.brand}</span>
-    </p>
-    <p className="text-gray-700 mb-2">
-      {product.rating} out of {product.numReviews} reviews
-    </p>
-    <p className="text-gray-700 mb-2">
-      Description: <span className='ml-3 text-lg'>{product.description}</span>
-    </p>
-    </div>
-    <div className="p-2">
-      <div className="flex justify-between items-center mb-4">
-        <span className="text-gray-700">Price:</span>
-        <span className="text-xl font-medium text-gray-800">
-          ${product.price}
-        </span>
-      </div>
-      <div className="flex justify-between items-center mb-4">
-        <span className="text-gray-700">Status:</span>
-        <span className={`text-lg font-medium ${product.countInStock > 0 ? 'text-green-600' : 'text-red-600'}`}>
-          {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
-        </span>
-      </div>
-      <button
-        className={`w-full py-3 rounded-lg text-white font-semibold ${product.countInStock === 0 ? 'bg-gray-500 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-600'}`}
-        onClick={addToCartHandler}
-        disabled={product.countInStock === 0}
-      >
-        {product.countInStock === 0 ? 'Out of Stock' : 'Add to Cart'}
-      </button>
-    </div>
-  </div>
-</div>
-
+        <Link href="/" className="text-gray-500 hover:text-gray-700">
+            &lt; Back to Products
+        </Link>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4 mb-5">
+        <div className="md:col-span-2">
+            <Image
+            src={product.image}
+            alt={product.name}
+            width={340}
+            height={540}
+            className="object-cover rounded-lg shadow-md mb-6"
+            />
+        </div>
+        <div className="col-span-3 md:col-span-2">
+        <h1 className="text-3xl font-bold text-gray-800 mb-4">{product.name}</h1>
+            <div className="p-2 text-sm">
+            <p className="text-gray-700 mb-2">
+            Category: <span className='ml-3 text-lg'>{product.category}</span>
+            </p>
+            <p className="text-gray-700 mb-2">
+            Brand: <span className='ml-3 text-lg'>{product.brand}</span>
+            </p>
+            <p className="text-gray-700 mb-2">
+            {product.rating} out of {product.numReviews} reviews
+            </p>
+            <p className="text-gray-700 mb-2">
+            Description: <span className='ml-3 text-lg'>{product.description}</span>
+            </p>
+            </div>
+            <div className="p-2">
+            <div className="flex justify-between items-center mb-4">
+                <span className="text-gray-700">Price:</span>
+                <span className="text-xl font-medium text-gray-800">
+                ${product.price}
+                </span>
+            </div>
+            <div className="flex justify-between items-center mb-4">
+                <span className="text-gray-700">Status:</span>
+                <span className={`text-lg font-medium ${product.countInStock > 0 ? 'text-green-600' : 'text-red-600'}`}>
+                {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                </span>
+            </div>
+            <button
+                className={`w-full py-3 rounded-lg text-white font-semibold ${product.countInStock === 0 ? 'bg-gray-500 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-600'} disabled:bg-blue-300`}
+                onClick={addToCartHandler}
+                disabled={product.countInStock === 0 || product.countInStock < 21}
+            >
+                {product.countInStock === 0 ? 'Out of Stock' : 'Add to Cart'}
+            </button>
+            </div>
+        </div>
+        </div>
     </Layout>
   );
 }
