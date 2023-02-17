e6a01a3e2e2c95b808e13a518623a3fbca9545a7
mohammedBnomr <moohaboy@gmail.com>
2023-02-17 14:57:28 +0300

    establish database connection

---
+++ pages/api/seed.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+// import Product from '../../models/Product';
+// import User from '../../models/User';
+import data from '../../utils/data';
+import db from '../../utils/db';
+
+interface Data {
+  message: string
+}
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) => {
+  await db.connect();
+  // await User.deleteMany();
+  // await User.insertMany(data.users);
+  // await Product.deleteMany();
+  // await Product.insertMany(data.products);
+  await db.disconnect();
+  res.send({ message: 'seeded successfully' });
+};
+export default handler;
