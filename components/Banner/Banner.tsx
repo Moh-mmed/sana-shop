740f9eaced8089dac2ad584459f57b80fa85298b
mohammedBnomr <moohaboy@gmail.com>
2023-02-22 15:10:11 +0300

    add banner to homepage

---
+++ components/Banner/Banner.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import React from "react";
+import s from './Banner.module.css'
+
+const Banner: React.FC = () => {
+  
+  return (
+    <section>
+          <div className={s.bannerContainer}>
+              <div className={s.bannerContent}>
+                <div>
+                  <span className={s.bannerContent_head}>Sale</span>
+                </div>
+
+                <div>
+                  <h2 className={s.bannerContent_body}>online and in stores</h2>
+                </div>
+
+                <div>
+                  <Link href="/shop" className={s.bannerContent_button}>
+                      Shop Now
+                  </Link>
+                </div>
+              </div>
+          </div>
+    </section>
+  );
+};
+
+
+export default Banner;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { GetServerSideProps, NextPage } from 'next'
 import React from 'react'
-import Layout from '../components/Layout'
+import Layout from '../components/Layout/Layout'
 import ProductItem from '../components/ProductItem'
 import { ProductTypes } from '../types/DataTypes'
 import { useDispatch, useSelector } from "react-redux";
@@ -8,13 +8,14 @@ import { addProduct } from "../redux/cartSlice";
 import { StoreTypes } from '../types/StoreTypes'
 import axios from 'axios'
 import { toast } from 'react-toastify'
+import Banner from '../components/Banner/Banner'
 
 type PropsTypes = {
     products: ProductTypes[],
     featuredProducts: ProductTypes[]
 }
 
-const Home: NextPage<PropsTypes> = ({ products, featuredProducts }) => {
+const Homepage: NextPage<PropsTypes> = ({ products, featuredProducts }) => {
   const {cart} = useSelector((state:StoreTypes) => state);
   const dispatch = useDispatch();
 
@@ -31,6 +32,12 @@ const Home: NextPage<PropsTypes> = ({ products, featuredProducts }) => {
   
   return (
     <Layout title="Home" description='Home page for sana shop'>
+      <Banner/>
+      {/* <CategoryPicker />
+      <FeaturedProducts data={storeOverview} />
+      <FeaturedBlogs data={blogOverview} /> */}
+
+
       <h2 className="h2 my-4">Latest Products</h2>
       <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {products.map((product) => (
@@ -45,17 +52,17 @@ const Home: NextPage<PropsTypes> = ({ products, featuredProducts }) => {
   )
 }
 
-export default Home
+export default Homepage
 
 
 
 export const getServerSideProps:GetServerSideProps = async() =>{
   const products = await axios.get(`${process.env.ROOT_URL}/api/products`);
-  // const featuredProducts = await Product.find({ isFeatured: true }).lean();
+  const featuredProducts = await axios.get(`${process.env.ROOT_URL}/api/products?featured=true`);
 
   return {
     props: {
-      featuredProducts: products.data.data,
+      featuredProducts: featuredProducts.data.data,
       products: products.data.data,
     },
   };
