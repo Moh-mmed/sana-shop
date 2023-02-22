e08627f5401969a9e1f3a406acfacd98726bc3b1
mohammedBnomr <moohaboy@gmail.com>
2023-02-22 13:47:40 +0300

    update footer

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,106 @@
+import React from "react";
+import { FaFacebook, FaTwitter, FaInstagram, FaStripe, FaCcMastercard } from "react-icons/fa";
+import {  RiMastercardFill, RiVisaFill } from "react-icons/ri";
+import {  BsPaypal } from "react-icons/bs";
+import s from './Footer.module.css'
+import Link from "next/link";
+
+const Footer:React.FC = () => {
+  return (
+    <footer className={s.root}>
+		<div className={s.container}>
+			<div className={s.infoPart}>
+				<div className={s.helpSection}>
+					<h4 className={s.sectionTitle}>
+						Help
+					</h4>
+					<ul>
+						<li className="pb-2">
+							<Link href="#" className={s.helpSection_link}>
+								Track Order
+							</Link>
+						</li>
+
+						<li className="pb-2">
+							<Link href="#" className={s.helpSection_link}>
+								Returns
+							</Link>
+						</li>
+
+						<li className="pb-2">
+							<Link href="#" className={s.helpSection_link}>
+								Shipping
+							</Link>
+						</li>
+
+						<li className="pb-2">
+							<Link href="#" className={s.helpSection_link}>
+								FAQs
+							</Link>
+						</li>
+					</ul>
+				</div>
+
+				<div className={s.getInTouchSection}>
+					<h4 className={s.sectionTitle}>
+						GET IN TOUCH
+					</h4>
+
+					<p className={s.getInTouchSection_body}>
+						Any questions? Let us know in store at 8th floor, 379 Hudson St, New York, NY 10018 or call us on (+1) 96 716 6879
+					</p>
+
+					<div className={s.socialContainer}>
+						<Link href="https://facebook.com" target={'_blank'} className={s.socialLink}>
+							<FaFacebook />
+						</Link>
+
+						<Link href="https://twitter.com" target={'_blank'} className={s.socialLink}>
+							<FaTwitter />
+						</Link>
+
+						<Link href="https://instagram.com" target={'_blank'} className={s.socialLink}>
+							<FaInstagram />
+						</Link>
+					</div>
+				</div>
+
+				<div className={s.newsletterSection}>
+					<h4 className={s.sectionTitle}>
+						Newsletter
+					</h4>
+
+					<form>
+						<div className={s.newsletterSection_inputWrapper}>
+							<input className={s.newsletterSection_input} type="text" name="email" placeholder="email@example.com" />
+							<div className={s.newsletterSection_inputLine}></div>
+						</div>
+
+						<div className="pt-4">
+							<button className={s.newsletterSection_button}>
+								Subscribe
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+
+      <div className={s.paymentMethodsPart}>
+          <RiVisaFill className={s.paymentIcon} />
+          <BsPaypal className={s.paymentIcon}/>
+          <FaStripe className={s.paymentIcon}/>
+          <FaCcMastercard className={s.paymentIcon}/>
+          <RiMastercardFill className={s.paymentIcon}/>
+      </div>
+        
+      <div className={s.rightsPart}>
+        <p>
+          &copy; 2023 Sana Shop. All rights reserved.
+        </p>
+      </div>
+		</div>
+	  </footer>
+  );
+};
+
+export default Footer;
--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,9 +1,15 @@
 import Head from "next/head";
-import Footer from "./Footer";
-import Navbar from "./Navbar";
-import LayoutTypes from "../types/LayoutTypes";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
 import { Flip, ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import s from './Layout.module.css'
+
+type LayoutTypes  = {
+    children: React.ReactNode;
+    title?: string,
+    description?: string
+}
 
 const Layout:React.FC<LayoutTypes> = ({ children, title,description }) => {
   return (
@@ -15,9 +21,9 @@ const Layout:React.FC<LayoutTypes> = ({ children, title,description }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <ToastContainer position="bottom-center" transition={Flip} limit={1} autoClose={1000} />
-      <div className="flex min-h-screen flex-col justify-between">
-        <Navbar />
-        <main className="bg-white flex-1 px-10 py-10">
+      <div className={s.root}>
+        <Header />
+        <main className={s.main}>
         {children}
         </main>
         <Footer />
