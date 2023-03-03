0307d19d9defc1afaa8e69b5f4d89ac499a1eb1f
mohammedBnomr <moohaboy@gmail.com>
2023-03-03 11:46:14 +0300

    list all users

--- pages/api/admin/users/index.ts
+++ pages/api/admin/users/index.ts
@@ -18,23 +18,37 @@ interface User {
   isAdmin?: boolean
 }
 
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const session = await getSession({ req }) as Session & { user: User };
+  // const session = await getSession({ req })
+  // if (!session || !session.user.isAdmin) {
+  //   return res.status(401).json({ status: "fail", message: 'admin sign in required' });
+  // }
+  const { method } = req;
 
-  if (!session || !session.user?.isAdmin) {
-    return res.status(401).json({ status: "fail", message: 'admin sign in required' });
+ switch (method) {
+   case 'GET': {
+    
+    try {
+        await db.connect();
+        const users = await User.find();
+        await db.disconnect();
+      
+        return res.status(200).json({
+          status: "success",
+          message: "All users have been fetched successfully",
+          data: users
+        });
+      
+      } catch (error) {
+          return res.status(500).json({ status: "fail", message: error });
+      }
+   }
+    default:{
+          return res.status(405).json({ status: "fail", message: 'Method not allowed' });
+    }
   }
-
-  await db.connect();
-  const users = await User.find();
-  await db.disconnect();
-  return res.status(200).json({
-      status: "success",
-      message: "All users have been fetched successfully",
-      data: users
-  });
+  
 };
