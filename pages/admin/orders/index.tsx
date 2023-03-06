7f6881082fb9c38dff354c94ef077c278e853879
mohammedBnomr <moohaboy@gmail.com>
2023-03-06 15:34:18 +0300

    add deliver functionality to admin

--- pages/admin/orders/index.tsx
+++ pages/admin/orders/index.tsx
@@ -1,39 +1,53 @@
 import axios from 'axios';
 import { NextPage } from 'next';
-import { Session } from 'next-auth';
-import { getSession } from 'next-auth/react';
 import { useEffect,  useState } from 'react';
 import Layout from '../../../components/admin/Layout/Layout';
 import s from '../../../styles/admin/Orders.module.css'
-import { getError } from '../../../utils/error';
 import LoadingSpinner from '../../../utils/components/LoadingSpinner';
 import { OrderTypes } from '../../../types/OrderTypes';
-import { UserTypes } from '../../../types/UserTypes';
 import { format } from 'date-fns';
-import { getSuccessStyles } from '../../../utils/helpers';
-import Link from 'next/link';
+import LoadingButton from '../../../utils/components/LoadingButton';
+import { toast } from 'react-toastify';
+import { getError } from '../../../utils/error';
 
-const AdminOrders: NextPage = ({admin}:any) => {
+const AdminOrders: NextPage = () => {
   const [orders, setOrders] = useState<OrderTypes[]>([])
   const [loading, setLoading] = useState(false)
+  const [loadingDeliver, setLoadingDeliver] = useState(false)
 
-  useEffect(() => {
-    const fetchData = async () => {
+  const fetchData = async () => {
     setLoading(true)
     try {
       const { data } = await axios.get(`/api/admin/orders`);
-      setOrders(data.data.filter((order: OrderTypes) => order._id !== admin._id))
+      setOrders(data.data)
       setLoading(false)
 
     } catch (err) {
-      console.log(getError(err))
       setLoading(false)
     }
-    
-    }
-  fetchData()
+  
+  }
+  useEffect(() => {
+    fetchData()
   }, []);
   
+
+  async function deliverOrderHandler(orderId:string) {
+    try {
+      setLoadingDeliver(true)
+      const { data } = await axios.put(
+        `/api/admin/orders/${orderId}/deliver`,
+        {}
+      );
+      setLoadingDeliver(false)
+      fetchData()
+      toast.success(data.message);
+    } catch (err) {
+      setLoadingDeliver(false)
+      toast.error(getError(err));
+    }
+  }
+
   return (
     <Layout title="Admin Orders">
       {loading ?
@@ -67,14 +81,13 @@ const AdminOrders: NextPage = ({admin}:any) => {
                           DELIVERED
                       </th>
                       <th scope="col" className="px-6 py-3">
-                          <span className="sr-only">Actions</span>
-                </th>
-                 
+                        <span className="sr-only">Actions</span>
+                      </th>
                   </tr>
               </thead>
               <tbody>
                 {orders.map((order:any, index) => (
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" key={order._id}>
+                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" key={index}>
                       <td scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                           {order._id.substring(0, 10)}...
                       </td>
@@ -89,18 +102,28 @@ const AdminOrders: NextPage = ({admin}:any) => {
                       </td>
                       <td className={s.cell}>
                           {order.isPaid
-                      ? <span className={getSuccessStyles(true)}>{format(new Date(order.paidAt), 'dd MMM yyyy: p')}</span>
-                      : <span className={getSuccessStyles(false)}>not paid</span>}
+                      ? <span className='success'>{format(new Date(order.paidAt), 'dd MMM yyyy: p')}</span>
+                      : <span className='fail'>not paid</span>}
                       </td>
                       <td className={s.cell}>
                           {order.isDelivered
-                      ? <span className={getSuccessStyles(true)}>{format(new Date(order.deliveredAt), 'dd MMM yyyy: p')}</span>
-                      : <span className={getSuccessStyles(false)}>not delivered</span>}
+                      ? <span className='success'>{format(new Date(order.deliveredAt), 'dd MMM yyyy: p')}</span>
+                      : <span className='fail'>not delivered</span>}
                       </td>
                       <td className={`${s.cell} ${s.actionCell}`}>
-                        <Link href={`/order/${order._id}`} className={`${s.actionBtn} ${s.showBtn}`}>
-                         details
-                       </Link>
+                        <button 
+                          className={`${s.actionBtn} ${s.viewBtn}`}
+                          // onClick={() => viewEditUser(user._id, 'view')}
+                        >
+                          view
+                        </button>
+                        <button
+                          disabled={loadingDeliver}
+                          className={`${s.actionBtn} ${s.deliverBtn}`}
+                          onClick={()=>deliverOrderHandler(order._id)}
+                        >
+                          deliver
+                        </button>
                       </td>
                     </tr>
                   ))}
@@ -111,24 +134,4 @@ const AdminOrders: NextPage = ({admin}:any) => {
     </Layout>
   );
 }
-
-export const getServerSideProps = async (context:any) => {
-  const session = await getSession(context) as Session & { user: UserTypes };
-
-  if (!session || !session.user.isAdmin) {
-    return {
-      redirect: {
-        destination: '/unauthorized',
-        permanent: false,
-      },
-    };
-  }
-  
-  return {
-    props: {
-      admin: session.user
-    },
-  };
-}
-
 export default AdminOrders;
