f9940d4dde65eadb3c1b697e5fc2f78e68471e25
mohammedBnomr <moohaboy@gmail.com>
2023-03-07 15:10:35 +0300

    fix bug

--- pages/api/admin/products/[id].ts
+++ pages/api/admin/products/[id].ts
@@ -10,85 +10,75 @@ export default async function handler(
   const { method, query, body } = req;
   const { id } = query
   
-  
+  try {
     switch (method) {
       case 'GET': {
-        try {
-          await db.connect();
-          const product = await Product.findById(id);
-          await db.disconnect();
-          
-          if (!product) {
-            return res.status(404).json({ status: "fail", message: 'Product not found' });
-          }
-
-          return res.status(200).json({
-            status: "success",
-            message: "Product has been fetched successfully",
-            data: product
-          });
-        } catch (error) {
-          return res.status(500).json({ status: "fail", message: error });
+        await db.connect();
+        const product = await Product.findById(id);
+        await db.disconnect();
+        
+        if (!product) {
+          return res.status(404).json({ status: "fail", message: 'Product not found' });
         }
+
+        return res.status(200).json({
+          status: "success",
+          message: "Product has been fetched successfully",
+          data: product
+        });
       }
          
       case 'PUT': {
-        try {
-          await db.connect();
-          const product = await Product.findById(id);
-          
-          if (!product) {
-            await db.disconnect();
-            return res.status(404).json({ status: "fail", message: 'Product not found' });
-          }
-
-          product.name = body.name;
-          product.brand = body.brand;
-          product.category = body.category;
-          product.gender = body.gender;
-          product.price = body.price;
-          product.countInStock = body.countInStock;
-          product.description = body.description;
-          product.isFeatured = body.isFeatured;
-          if (body.image) {
-            product.banner = body.image;
-            product.image = body.image;
-          }
-
-          await product.save();
+        await db.connect();
+        const product = await Product.findById(id);
+        
+        if (!product) {
           await db.disconnect();
-          
-          return res.status(202).json({
-            status: "success",
-            message: "Product updated successfully",
-            data: product
-          });
-        } catch (error) {
-          return res.status(500).json({ status: "fail", message: error });
+          return res.status(404).json({ status: "fail", message: 'Product not found' });
         }
+
+        product.name = body.name;
+        // product.brand = body.brand;
+        // product.category = body.category;
+        // product.gender = body.gender;
+        // product.price = body.price;
+        // product.countInStock = body.countInStock;
+        // product.description = body.description;
+        // product.isFeatured = body.isFeatured;
+        // if (body.image) {
+        //   product.banner = body.image;
+        //   product.image = body.image;
+        // }
+        
+        await product.save();
+        await db.disconnect();
+        
+        return res.status(202).json({
+          status: "success",
+          message: "Product updated successfully",
+          data: product
+        });
       }   
 
       case 'DELETE': {
-        try {
-          await db.connect();
-          const product = await Product.findById(id);
-          
-          if (!product) { 
-            await db.disconnect();
-            return res.status(404).json({ status: "fail", message: 'Product not found' });
-          }
-          await product.remove();
+        await db.connect();
+        const product = await Product.findById(id);
+        
+        if (!product) { 
           await db.disconnect();
-
-          return res.status(201).json({ status: "success", message: 'Product deleted successfully' });
-          
-        } catch (error) {
-          return res.status(500).json({ status: "fail", message: error });
+          return res.status(404).json({ status: "fail", message: 'Product not found' });
         }
+        await product.remove();
+        await db.disconnect();
+
+        return res.status(201).json({ status: "success", message: 'Product deleted successfully' });
       }
 
       default:{
         return res.status(405).json({ status: "fail", message: 'Method not allowed' });
       }
     }
+  } catch (error) {
+    return res.status(500).json({ status: "fail", message: error });
+  }
 };
--- pages/api/admin/products/index.ts
+++ pages/api/admin/products/index.ts
@@ -10,64 +10,73 @@ export default async function handler(
 
   const { method, body } = req;
 
+  try {
     switch (method) {
       case 'GET': {
-        try {
-          await db.connect();
-          const products = await Product.find().sort({ updated_at: -1 });
-          await db.disconnect();
-        
-          return res.status(200).json({
-            status: "success",
-            message: "All products have been fetched successfully",
-            data: products
-          });
-          } catch (error) {
-            return res.status(500).json({ status: "fail", message: error });
-        }
+        await db.connect();
+        const products = await Product.find().sort({ updated_at: -1 });
+        await db.disconnect();
+      
+        return res.status(200).json({
+          status: "success",
+          message: "All products have been fetched successfully",
+          data: products
+        });
       }
       case 'POST': {
-        try {
-          const {
-            name,
-            category,
-            brand,
-            gender,
-            price,
-            countInStock,
-            description,
-            isFeatured,
-            image,
-          } = body;
-          
-          await db.connect();
+        const {
+          name,
+          category,
+          brand,
+          gender,
+          price,
+          countInStock,
+          description,
+          isFeatured,
+          image,
+        } = body;
+        
+        await db.connect();
 
-          const product = await Product.create({
-            name,
-            image,
-            price,
-            gender,
-            banner:image,
-            category,
-            brand,
-            isFeatured,
-            countInStock,
-            description,
-          });
+        // const product = await Product.create({
+        //   name,
+        //   image,
+        //   price,
+        //   gender,
+        //   banner:image,
+        //   category,
+        //   brand,
+        //   isFeatured,
+        //   countInStock,
+        //   description,
+        // });
+        const product = await Product.create({
+            name: "Esprit Ruffle Shirt",
+            category: "t-shirt",
+            gender: "women",
+            image: "https://res.cloudinary.com/dbut5ydes/image/upload/v1678178099/sanashop/product-01_fxsjz0.jpg",
+            price: 86.45,
+            brand: "Esprit Ruffle Shirt",
+            rating:4.8,
+            numReviews: 40,
+            countInStock: 30,
+            isFeatured: false,
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        });
 
-          await db.disconnect();
+        await db.disconnect();
 
-          return res.status(202).json({
-            status: "success",
-            message: "Product created successfully",
-            data: product
-          });
-        } catch (error) {
-          return res.status(500).json({ status: "fail", message: error });
-        }
+        return res.status(202).json({
+          status: "success",
+          message: "Product created successfully",
+          data: product
+        });
       }
       default:{
         return res.status(405).json({ status: "fail", message: 'Method not allowed' });
       }
     }
+  } catch (error) {
+      return res.status(500).json({ status: "fail", message: error });
+  }
 };
