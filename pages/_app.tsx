2007e1b25c5480df52291bf72ea37181fbc500ab
mohammedBnomr <moohaboy@gmail.com>
2023-04-02 17:52:44 +0300

    clean up _app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,33 +15,8 @@ export default function App({ Component, pageProps:{session, ...pageProps} }: Ap
       <Provider store={store}>
         <PayPalScriptProvider options={SCRIPT_PROVIDER_OPTIONS} deferLoading={true}>
         <Component {...pageProps} />
-        {/* {Component.auth ? (
-            <Auth adminOnly={Component.auth.adminOnly}>
-              <Component {...pageProps} />
-            </Auth>
-          ) : (
-            <Component {...pageProps} />
-          )} */}
         </PayPalScriptProvider>
       </Provider>
     </SessionProvider>
   )
 }
-
-// function Auth({ children, adminOnly }:any) {
-//   const router = useRouter();
-//   const { status, data: session } = useSession({
-//     required: true,
-//     onUnauthenticated() {
-//       router.push('/unauthorized?message=login required');
-//     },
-//   });
-//   if (status === 'loading') {
-//     return <div>Loading...</div>;
-//   }
-//   if (adminOnly && !session.user.isAdmin) {
-//     router.push('/unauthorized?message=admin login required');
-//   }
-
-//   return children;
-// }
